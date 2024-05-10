9b37af47a6ea98e4cf29d15dbd6dcceca767e972
jainoddin <skjainoddin39854@gmail.com>
2024-05-10 15:02:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <form action="?" method="POST">
+      <div class="g-recaptcha" data-sitekey="6LcKwNcpAAAAAG4B3zQwEL7vsvqbOa0zQvvdfUFC"></div>
+      <br/>
+      <input type="submit" value="Submit"/>
+    </form>
+    </>
   );
 }
 
